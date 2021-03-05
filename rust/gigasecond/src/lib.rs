a7153cec189c87c8a531c7f18b95e7a2f8976393
Ebuka Agbanyim <ebuka7@outlook.com>
2021-03-05 22:44:45 +0000

    better solution

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -2,5 +2,11 @@ use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(1_000_000_000)
+    // old solution
+    // start + Duration::seconds(1_000_000_000)
+
+    // good solution that checks for overflow and panics if it does to prevent undefined behaviour.
+    start
+        .checked_add_signed(Duration::seconds(1_000_000_000))
+        .expect("overflow")
 }
