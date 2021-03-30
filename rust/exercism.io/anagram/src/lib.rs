648ba47601493fd97f7a3948b4c2bb22262bc191
Ebuka Agbanyim <ebuka7@outlook.com>
2021-03-30 21:22:39 +0100

    Add community solution

--- rust/exercism.io/anagram/src/lib.rs
+++ rust/exercism.io/anagram/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a, 'b>(word: &'a str, possible_anagrams: &[&'b str]) -> HashSet<&'b str> {
+pub fn anagrams_for_2<'a, 'b>(word: &'a str, possible_anagrams: &[&'b str]) -> HashSet<&'b str> {
     possible_anagrams
         .iter()
         .filter(|&test_word| {
@@ -22,5 +22,34 @@ pub fn anagrams_for<'a, 'b>(word: &'a str, possible_anagrams: &[&'b str]) -> Has
             false
         })
         .cloned()
+        .collect()
+}
+
+fn get_sorted(word: &str) -> Vec<char> {
+    let mut word_sorted: Vec<char> = word.chars().collect();
+    word_sorted.sort_unstable();
+    word_sorted
+}
+
+// suggested community mentor solution, slightly better performance compared to above:
+// 3,001 ns/iter (+/- 88) vs 2,495 ns/iter (+/- 87)
+//
+// Clone is explicit and might be expensive and Copy is implicit and cannot be re-implemented
+// in the below case because we are returning reference to values that already exist and must live
+// to satisfy the lifetime of the return variable, it makes sense to do a .copied() operation
+// instead of a .clone() on line 52. This could explain the diffrence in performance in community
+// solution.
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word_lower = word.to_lowercase();
+    let word_sorted = get_sorted(&word_lower);
+    possible_anagrams
+        .iter()
+        .filter(|candidate| {
+            let candidate_lower = candidate.to_lowercase();
+            candidate_lower.len() == word_lower.len() // lengths must be equal.
+                && candidate_lower != word_lower // words are not initially the same.
+                && get_sorted(&candidate_lower) == word_sorted // anagram contains all of word.
+        })
+        .copied()
         .collect()
 }
